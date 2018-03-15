e2f2bd6f9cc5c9c79f573423dc5a0430536b2b96
Pratap Lakshman <pratapl@microsoft.com>
2018-03-15 10:28:30 +0530

    Initital commit.

---
+++ MyApp/MyTests/MathTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MyApp;
+
+namespace MyTests
+{
+    public class MathTests
+    {
+        [Fact]
+        public void addTest1()
+        {
+            int x = 5;
+            int y = 6;
+            int sum = 11;
+
+            var res = new MyApp.Math().add(x, y);
+            Assert.Equal(sum, res);
+        }
+    }
+}

932083c51011b1575aa6df6c777428a55f6fe504
Pratap Lakshman <pratapl@microsoft.com>
2018-03-16 16:10:27 +0530

    Added mult.cs

---
+++ MyApp/MyApp/Mult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class Mult
+    {
+        public int multiply(int x, int y)
+        {
+            var m = new MyApp.Math();
+
+            for (int k = 0; k < y; k++)
+            {
+                x = m.add(x, y);
+            }
+
+            return x;
+        }
+    }
+}

778590874906287f7cefee7e0ad9e50440061241
Pratap Lakshman <pratapl@microsoft.com>
2018-03-16 17:37:35 +0530

    Added mult tests.

---
+++ MyApp/MyTests/MultTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MyApp;
+
+
+namespace MyTests
+{
+    public class MultTests
+    {
+        [Fact]
+        public void multTest1()
+        {
+            int x = 5;
+            int y = 6;
+            int res = 30;
+
+            var val = new MyApp.Mult().multiply(x, y);
+            Assert.Equal(res, val);
+        }
+    }
+}

dc0994c117eba4ed7178bcd3420b041d4c202d2a
Pratap Lakshman <pratapl@microsoft.com>
2018-07-26 16:48:48 +0530

    added div and div tests

---
+++ MyApp/MyApp/Div.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class Div
+    {
+        public int div2(int x, int y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException();
+
+            return x / y;
+        }
+    }
+}

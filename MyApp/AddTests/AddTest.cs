649ac8ce447b78cc32aa07a80cb3b0cd8016ef5e
Pratap Lakshman <pratapl@microsoft.com>
2018-07-27 11:31:27 +0530

    Added back 2 tests.

--- MyApp/AddTests/AddTest.cs
+++ MyApp/AddTests/AddTest.cs
@@ -9,12 +9,12 @@ namespace AddTests
         [Fact]
         public void addTest1()
         {
-            //int x = 5;
-            //int y = 6;
-            //int sum = 11;
+            int x = 5;
+            int y = 6;
+            int sum = 11;
 
-            //var res = new AddOp.Add().add(x, y);
-            //Assert.Equal(sum, res);
+            var res = new AddOp.Add().add(x, y);
+            Assert.Equal(sum, res);
         }
     }
 }
